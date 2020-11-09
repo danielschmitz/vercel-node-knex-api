e77d611bcb667298cb92d5e93e9dc26840b061ac
Daniel Schmitz <danieljfa@gmail.com>
2020-11-09 20:28:56 -0300

    multiple envs

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
 require('dotenv').config()
+
+const env = process.env.NODE_ENV || "development"
+
 const knexfile = require('./knexfile')
-module.exports = require('knex')(knexfile)
+module.exports = require('knex')(env)
--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,41 @@
 require('dotenv').config()
 
-
 module.exports = {
-  client: 'postgresql',
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-}
 
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL_DEVELOPMENT,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL_STAGING,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL_PRODUCTION,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
